f2f32b1aef818fa7a39c6b808cc19b14562bde2c
Danan Subramaniam <danan_varo@hotmail.com>
2025-11-09 17:54:55 +0100

    Created and tested API call to Azure. Missing API call to frontend.

--- backend/ai_bridge.py
+++ backend/ai_bridge.py
@@ -1,12 +1,56 @@
+from typing import Optional
 from dotenv import load_dotenv
 import os
-from fastapi import FastAPI
 
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from azure.ai.projects import AIProjectClient
+from azure.identity import DefaultAzureCredential
+from azure.ai.agents.models import ListSortOrder
+
+
+#Loads API keys and agent id
 load_dotenv()
 
+# Creates and api from the fastapi library
 app = FastAPI()
 
+# Endpoints and agents
 azureEndpoint = os.getenv("azureEndpoint")
+azureAgent = os.getenv("getAgent")
 
-agent = os.getenv("getAgent")
+# Creates a AI Client
+project = AIProjectClient(
+    credential= DefaultAzureCredential(),
+    endpoint=azureEndpoint
+)
 
+class ChatRequest(BaseModel):
+    input: str
+    threadID: Optional[str] = None
+
+class ChatResponse(BaseModel):
+    response: str
+    threadID: str
+
+## testing the azure api
+
+
+"""
+
+
+"""
+
+app.post("/chat",response_model=ChatResponse)
+def getChat(request : ChatRequest): 
+
+    # Checks if thread exists if not creates a new one
+    if request.threadID == None:
+        
+        thread =project.agents.threads.create()
+        threadID = thread.id()
+    else:
+        threadID = request.threadID
+
+    project.agents.threads
